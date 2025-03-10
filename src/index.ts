8342ac2309270a5ab37855e689194aba51717d09
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-03-10 00:26:22 -0500

    refactor(config): updates initial setup

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export type * from './domain/types';
-export * from './domain/models';
-export * from './domain/adapters';
-
-export * from './hooks/useAccessTokenSilently';
-
-export * from './context/AuthSilently';
